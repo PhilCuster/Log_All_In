3b102b62d68831e662c09f35780ba197072c2b3c
PhilCuster <vengevine763>
2015-11-30 17:46:58 -0500

    Changed the way that users are stored and checked/registered.  Users are stored in ext. file and are maintained between server runs...

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ __author__ = 'Phil'
 import hashlib
 import datetime
 import time
+import users
 from flask import Flask, send_from_directory, request, url_for, redirect, render_template
 
 app = Flask(__name__, static_url_path='')
 
 encoded_salt = b"example"
 
-# TEST USERS, DO NOT INCLUDE IN PRODUCTION!!!!!!!!!!!!!!!!
-user_base = {}  #{'admin' : hashlib.sha256(b'password' + encoded_salt)}
-user_emails = {}
 
+user_base = []  #{'admin' : hashlib.sha256(b'password' + encoded_salt)}
 
 
 
@@ -20,27 +19,33 @@ def current_time():
     return datetime.datetime.fromtimestamp(time.time()).strftime('[%Y-%m-%d %H:%M:%S]')
 
 
+def write_log(message):
+    with open('log.txt', 'a') as log:
+        log.write(current_time() + message + '\n')
+
+
 def validate_login(username, password):
     encoded_pass = bytes(password, encoding='UTF-8')
-    try:
-        if hashlib.sha256(encoded_pass + encoded_salt).digest() == user_base[username.lower()].digest():
-            return True
-        else:
-            return False
-    except KeyError:
+    hashed_pass = hashlib.sha256(encoded_pass + encoded_salt).digest()
+    if users.attempt_login(user_base, username.lower(), hashed_pass):
+        write_log('Login from user: ' + username)
+        return True
+    else:
+        write_log('Attempted login from user: ' + username)
         return False
 
 
 def validate_registration(username, password, email):
     username = username.lower()
     encoded_pass = bytes(password, encoding='UTF-8')
-    if not username in user_base:
-        user_base[username] = hashlib.sha256(encoded_pass + encoded_salt)
-        user_emails[username] = email.lower()
-        print("Registered " + username + "!")
+    hashed_pass = hashlib.sha256(encoded_pass + encoded_salt).digest()
+    print("Attempting to register user: " + username)
+    if users.add_user(user_base, username, hashed_pass, email):
+        print('Added new user: ' + username)
+        write_log("New user added: " + username)
         return True
     else:
-        print("User already exists!")
+        print("Could not validate user...")
         return False
 
 
@@ -51,14 +56,11 @@ def root():
 
 @app.route('/index_submit', methods=['POST', 'GET'])
 def login():
-    error = None
     if request.method == 'POST':
         if validate_login(request.form['username'], request.form['password']):
-            return succesful_login(request.form['username'])
+            return render_template('hello.html', name=request.form['username'])
         else:
             print("Invalid login...")
-            log.write(current_time() + "Unsuccessful login from User '" + username + "'...")
-            error = 'Invalid username/password'
 
     return render_template('invalid.html', name=request.form['username'])
 
@@ -75,11 +77,6 @@ def register():
     return render_template('invalid.html', name=request.form['username'])
 
 
-def succesful_login(username):
-    print(username + " has logged in!")
-    log.write(current_time() + "User '" + username + "' has logged in.")
-    return render_template('hello.html', name=username)
-
 if __name__ == '__main__':
-    log = open('log.txt', 'a')
+    user_base = users.populate_users()
     app.run()
