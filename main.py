ca93abcdcc124683fd149a296d6822e99a3499e5
Phil Custer <custer55@gmail.com>
2015-12-02 19:36:56 -0500

    AES implemented to encrypt user secrets.  Manual key entry required on server start.  Verification email now sent on registration.  EMail link does not work yet.

--- main.py
+++ main.py
@@ -4,12 +4,22 @@ import datetime
 import time
 import sqlite3
 import bcrypt
-from Crypto.Cipher import DES
+import base64
+import smtplib
+import random
+import string
+from Crypto.Cipher import AES
+from Crypto import Random
 from flask import Flask, request, url_for, redirect, render_template
 
 app = Flask(__name__, static_url_path='')
 
-encoded_salt = b"example"
+# Temporary storage for AES encryption key
+AES_KEY = None
+
+SITE_URL = "http://127.0.0.1:5000/"
+SITE_EMAIL = "logallin3@gmail.com"
+SITE_PASSWORD = "blueberry3"
 
 obj = None
 
@@ -22,15 +32,43 @@ def write_log(message):
         log.write(current_time() + ' ' + message + '\n')
 
 
+def send_email(target, code):
+    message = "Thank you for registering, please click this link to verify your account: " + SITE_URL + "/" + code
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    print('1')
+    server.ehlo()
+    print('2')
+    server.starttls()
+    print('3')
+    server.login(SITE_EMAIL, SITE_PASSWORD)
+    print('4')
+    msg = "\r\n".join([
+            "From: " + SITE_EMAIL,
+            "To: " + target,
+            "Subject: Verify your account!",
+            "",
+            message
+            ])
+    print('5')
+    server.sendmail(SITE_EMAIL, target, msg)
+    print('6')
+    server.quit()
+
+
 def hide_secret(secret):
+    # Block size for AES
+    block_size = 16
+
     # Thanks to http://www.dlitz.net/software/pycrypto/
     # Append period to end of secret
     secret += '.'
     # Strings for DES must be a multiple of 8 in length
-    add_x = 8 - (len(secret) % 8)
+    add_x = block_size - (len(secret) % block_size)
     secret += 'X' * add_x
-    ciph = obj.encrypt(secret)
-    return ciph
+
+    init_vector = Random.new().read(AES.block_size)
+    cipher = AES.new(AES_KEY, AES.MODE_CBC, init_vector)
+    return base64.b64encode(init_vector + cipher.encrypt(secret))
 
 
 def unhide_secret(user):
@@ -39,7 +77,10 @@ def unhide_secret(user):
     c.execute("SELECT secret FROM users WHERE username = ?", (user,))
     secret = c.fetchone()
     secret = secret[0]
-    unhidden_secret = obj.decrypt(secret)
+    secret = base64.b64decode(secret)
+    init_vector = secret[:16]
+    cipher = AES.new(AES_KEY, AES.MODE_CBC, init_vector)
+    unhidden_secret = cipher.decrypt(secret[16:])
     unhidden_secret = unhidden_secret.decode("utf-8")
     unhidden_secret = unhidden_secret.split('.')
     unhidden_secret = unhidden_secret[:-1]
@@ -85,7 +126,7 @@ def validate_registration(username, password, email, secret):
         write_log("Registration Failure: User of name '" + req_username + "' already exists...")
         return False
 
-    c.execute("SELECT email FROM users WHERE email = ?", (req_username,))
+    c.execute("SELECT email FROM users WHERE email = ?", (req_email,))
 
     if c.fetchone():
         # Email already registered.
@@ -99,6 +140,13 @@ def validate_registration(username, password, email, secret):
     database.commit()
     database.close()
     write_log("Added user '" + req_username + "' to the database")
+
+    # Send email to user.
+    validate = ''.join(random.choice(string.ascii_uppercase) for i in range(16))
+    print("Sending email to " + req_email)
+    send_email(req_email, validate)
+    print("Email success!")
+
     return True
 
 
@@ -138,5 +186,5 @@ def register():
 
 
 if __name__ == '__main__':
-    obj = DES.new('abcd1234', DES.MODE_ECB)
+    AES_KEY = input("Enter AES key: ").encode('utf-8')
     app.run()
